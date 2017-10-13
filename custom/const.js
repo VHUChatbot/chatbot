a847ec7b8542b72ec3c92c2315652d14bc8503d0
vhuchatbot <itc.vhu@gmail.com>
2017-10-13 21:14:42 +0700

    cmmit 1

---
+++ custom/const.js
@@ -0,0 +1,31 @@
+module.exports = {
+	DEBUG : false,
+
+	//FACEBOOK TOKEN
+	FB_PAGE_VERIFY_TOKEN : 'vhu.edu.vn', // đặt 1 mã bất kỳ
+	FB_PAGE_ACCESS_TOKEN : 'EAAHJJCgtbsUBACd2uFK5RonFARf735l6D4oGXcSZBvnO0sp9m52LkxQFG4ZB0PGPRrNkSmyZAf3N1hCIZBGztIYMsh21T6ZAEg9BJSfqZBBhZBBBuIaKA7K9mVOCaHHNVSLZAYXrcZCcVkdXaMFsdDRdLMcCmSpNAIfbQRb50B315CwZDZD',
+
+	//HEROKU STUFFS
+	APP_NAME : 'vhuchatbot',
+	HEROKU_API_KEY : '9e71046c-491e-49e8-85d5-0723fd6bed02',
+
+	//MYSQL SETUP
+	DB_CONFIG : {
+		host     : 'us-cdbr-iron-east-05.cleardb.net',
+		user     : 'bdc8e7bdd18a84',
+		password : 'f2bb34b2',
+		database : 'heroku_4a834b0e53cbdad'
+	},
+
+	//ANALYTICS
+	HAS_POST_LOG : false,
+	POST_LOG_ID : '',
+	POST_LOG_ENTRY1 : '',
+	POST_LOG_ENTRY2 : '',
+
+	//GOOGLE FORMS
+	REPORT_LINK : "",
+
+	//other
+	MAX_PEOPLE_WAITROOM : 7
+};
